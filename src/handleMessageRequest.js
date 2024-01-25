6b6c73886e85ad103f921a9ed4738196a8d3a21a
John Edward Bearneza <133111613+edconeza@users.noreply.github.com>
2024-01-25 08:07:56 +0800

    Update handleMessageRequest.js

--- src/handleMessageRequest.js
+++ src/handleMessageRequest.js
@@ -4,62 +4,52 @@ const utils = require("../utils");
 const log = require("npmlog");
 
 module.exports = function (defaultFuncs, api, ctx) {
-	return function handleMessageRequest(threadID, accept, callback) {
-		if (utils.getType(accept) !== "Boolean") {
-			throw {
-				error: "Please pass a boolean as a second argument."
-			};
-		}
+    return function handleMessageRequest(threadID, callback) {
+        let resolveFunc = function () { };
+        let rejectFunc = function () { };
+        const returnPromise = new Promise(function (resolve, reject) {
+            resolveFunc = resolve;
+            rejectFunc = reject;
+        });
 
-		let resolveFunc = function () { };
-		let rejectFunc = function () { };
-		const returnPromise = new Promise(function (resolve, reject) {
-			resolveFunc = resolve;
-			rejectFunc = reject;
-		});
+        if (!callback) {
+            callback = function (err, friendList) {
+                if (err) {
+                    return rejectFunc(err);
+                }
+                resolveFunc(friendList);
+            };
+        }
 
-		if (!callback) {
-			callback = function (err, friendList) {
-				if (err) {
-					return rejectFunc(err);
-				}
-				resolveFunc(friendList);
-			};
-		}
+        const form = {
+            client: "mercury",
+            inbox: threadID, // Set inbox to the threadID directly
+        };
 
-		const form = {
-			client: "mercury"
-		};
+        defaultFuncs
+            .post(
+                "https://www.facebook.com/ajax/mercury/move_thread.php",
+                ctx.jar,
+                form
+            )
+            .then(utils.parseAndCheckLogin(ctx, defaultFuncs))
+            .then(function (resData) {
+                if (resData.error) {
+                    throw resData;
+                }
 
-		if (utils.getType(threadID) !== "Array") {
-			threadID = [threadID];
-		}
+                // Additional actions after accepting a message
+                // Send an emoji after accepting the message
+                const emoji = '👍';
+                api.sendMessage(emoji, threadID[0]); // Assuming you want to send the emoji to the first thread in the list
 
-		const messageBox = accept ? "inbox" : "other";
+                return callback();
+            })
+            .catch(function (err) {
+                log.error("handleMessageRequest", err);
+                return callback(err);
+            });
 
-		for (let i = 0; i < threadID.length; i++) {
-			form[messageBox + "[" + i + "]"] = threadID[i];
-		}
-
-		defaultFuncs
-			.post(
-				"https://www.facebook.com/ajax/mercury/move_thread.php",
-				ctx.jar,
-				form
-			)
-			.then(utils.parseAndCheckLogin(ctx, defaultFuncs))
-			.then(function (resData) {
-				if (resData.error) {
-					throw resData;
-				}
-
-				return callback();
-			})
-			.catch(function (err) {
-				log.error("handleMessageRequest", err);
-				return callback(err);
-			});
-
-		return returnPromise;
-	};
+        return returnPromise;
+    };
 };
